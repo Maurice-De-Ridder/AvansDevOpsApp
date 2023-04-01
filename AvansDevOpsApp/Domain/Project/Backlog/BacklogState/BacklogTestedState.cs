7f025367798fcdb4085d358cdce958d3b25c06e1
Maurice <mauricederidder@outlook.com>
2023-04-01 12:32:17 +0200

    started state

--- AvansDevOpsApp/Domain/Project/Backlog/BacklogState/BacklogTestedState.cs
+++ AvansDevOpsApp/Domain/Project/Backlog/BacklogState/BacklogTestedState.cs
@@ -8,5 +8,9 @@ namespace AvansDevOpsApp.Domain.Project.Backlog.BacklogState
 {
     public class BacklogTestedState : IBacklogState
     {
+        public void ChangeState(IBacklogState State)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
